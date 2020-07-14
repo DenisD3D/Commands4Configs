11d269b0f8fdd2ac70ffb54bb5d8e32060ebe77d
DenisD3D <dd.denis2544@gmail.com>
2020-07-14 18:24:39 +0100

    1.0.1 Add missing types close #1

--- src/main/java/ml/denisd3d/commands4configs/Types.java
+++ src/main/java/ml/denisd3d/commands4configs/Types.java
@@ -12,7 +12,11 @@ public class Types
         types.put(Double.class, s -> NumberFormat.getInstance().parse(s));
         types.put(Integer.class, s -> NumberFormat.getInstance().parse(s));
         types.put(Float.class, s -> NumberFormat.getInstance().parse(s));
+        types.put(Short.class, s -> NumberFormat.getInstance().parse(s));
         types.put(Long.class, s -> NumberFormat.getInstance().parse(s));
+        types.put(Boolean.class, Boolean::parseBoolean);
+        types.put(Byte.class, Byte::parseByte);
+        types.put(Character.class, s-> s.charAt(0));
         types.put(String.class, s -> s);
     }
 }
